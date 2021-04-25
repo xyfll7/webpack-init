7fae9370580ed569ea2bef601b0e082a6d6b7ac9
小洋粉 <l.7@qq.com>
2021-04-25 08:59:43 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function component() {
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
-  console.log("aaaa");
   return element;
 }
 
